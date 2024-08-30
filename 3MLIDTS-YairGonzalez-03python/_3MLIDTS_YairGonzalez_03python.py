d4e8ea3ad7e967e1d97cadf8c92e75bc8d1946dc
ygbaldizon7 <54856417+ygbaldizon7@users.noreply.github.com>
2024-08-30 10:54:27 -0600

    Nuevos

--- 3MLIDTS-YairGonzalez-03python/_3MLIDTS_YairGonzalez_03python.py
+++ 3MLIDTS-YairGonzalez-03python/_3MLIDTS_YairGonzalez_03python.py
@@ -29,16 +29,11 @@ def guardar_valores():
     ### Generar la cadena de caracteres
     datos = "Nombres: "+ nombres +"\n"+"Apellidos: "+ apellidos +"\n"+"Edad: "+ edad +"años\n"+"Estatura: "+ estatura +"\n"+"Telefonos :"+ telefono +"\n"+"Genero: "+genero+"\n"
     ## Guardar los datos en el archivo TXT
-    with open("3O2024Datos.txt", "a") as archivo:
+    with open("C:\\Users\\ygbal\\Source\\Repos\\3N2024Datos.txt", "a") as archivo:
         archivo.write(datos+"\n\n")
     ###Mostrar mensaje de confirmacón
     messagebox.showinfo("Información", "Datos guardados con éxito: \n\n"+datos)
-    tbNombre.delete(0,tk.END)
-    tbApellidos.delete(0,tk.END)
-    tbEdad.delete(0,tk.END)
-    tbEstatura.delete(0,tk.END)
-    tbTelefono.delete(0,tk.END)
-    var_genero.set(0)
+    borrar_fun()
 
 ## Creación de Vetana
 ventana = tk.Tk()
@@ -49,6 +44,9 @@ var_genero = tk.IntVar()
 
 ## Creación de etiquetas y campos de entrada
 lbNombre= tk.Label(ventana, text="Nombres :")
+## Pack es el tercer metodo de ordenamiento de elementos graficos en python 
+#(1er- Place, 2do - Grid)
+# Ordenamiento por lista de aparicion
 lbNombre.pack()
 tbNombre = tk.Entry()
 tbNombre.pack()
